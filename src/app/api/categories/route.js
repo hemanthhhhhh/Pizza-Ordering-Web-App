9c696d14387daabe3b2c73be6566a81c7dcaa291
hemanthhhhhh <krishnahemant2005@gmail.com>
2023-12-30 19:06:05 +0530

    First commit

--- src/app/api/categories/route.js
+++ src/app/api/categories/route.js
@@ -1,18 +1,16 @@
 import mongoose from "mongoose"
 import { Category } from "../../../models/Category"
-import {isAdmin} from "@/app/api/auth/[...nextauth]/route";
+
 
 export async function POST(req) {
     mongoose.connect(process.env.MONGO_URL)
     const {name} = await req.json()
 
-    if(await isAdmin()) {
+
         const categoryDoc = await Category.create({name})
         return Response.json(categoryDoc)
-    }
-    else {
-        return Response.json({});
-    }
+
+    
 }
 
 export async function PUT(req) {
@@ -35,10 +33,9 @@ export async function DELETE(req) {
     mongoose.connect(process.env.MONGO_URL)
     const url = new URL(req.url)
     const _id = url.searchParams.get('_id')
-   
-    if(await isAdmin()) {
+
         await Category.deleteOne({_id})
-    }
+    
     return Response.json(true)
 }
 
