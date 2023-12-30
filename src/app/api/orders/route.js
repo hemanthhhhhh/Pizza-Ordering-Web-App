29acfd7c67ba189a7ff93e465f00d600b4d5c053
hemanthhhhhh <98905333+hemanthhhhhh@users.noreply.github.com>
2023-12-30 21:39:07 +0530

    Update route.js

--- src/app/api/orders/route.js
+++ src/app/api/orders/route.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import { getServerSession } from "next-auth";
-import {authOptions, isAdmin} from "@/app/api/auth/[...nextauth]/route";
+import {authOptions} from "@/app/api/auth/[...nextauth]/route";
 import {UserInfo} from '../../../models/UserInfo'
 import { Order } from "../../../models/Order";
 
@@ -9,17 +9,16 @@ export async function GET(req) {
     
     const session = await getServerSession(authOptions)
     const userEmail = session?.user?.email
-    const admin = await isAdmin()
+   
 
     const url = new URL(req.url);
     const _id = url.searchParams.get('_id')
     if(_id) {
         return Response.json(await Order.findById(_id))
     }
-
-    if(admin) {
-        return Response.json(await Order.find())
-    }
+    
+    return Response.json(await Order.find())
+    
     if(userEmail) {
         return Response.json(await Order.find({userEmail}))
     }
