82a8fc3db8f2eaf7f25c1bb99aafab991005dd7c
hemanthhhhhh <98905333+hemanthhhhhh@users.noreply.github.com>
2023-12-30 21:31:17 +0530

    Update route.js

--- src/app/api/menu-items/route.js
+++ src/app/api/menu-items/route.js
@@ -1,24 +1,20 @@
 import mongoose from "mongoose"
 import {MenuItem} from "@/models/MenuItems"
-import { isAdmin } from "../auth/[...nextauth]/route"
+
 
 export async function POST(req) {
     mongoose.connect(process.env.MONGO_URL)
     const data = await req.json()
-    if(await isAdmin()) {
-        const menuItemDoc = await MenuItem.create(data)
-        return Response.json(menuItemDoc)
-    }
-    else {
-        return Response.json({})
-    }
+   
+    const menuItemDoc = await MenuItem.create(data)
+    return Response.json(menuItemDoc)
 }
 export async function PUT(req) {
     mongoose.connect(process.env.MONGO_URL)
-    if(await isAdmin()) {
-        const {_id, ...data} = await req.json()
-        await MenuItem.findByIdAndUpdate(_id, data)
-    }
+   
+    const {_id, ...data} = await req.json()
+    await MenuItem.findByIdAndUpdate(_id, data)
+    
     return Response.json(true)
 }
 export async function GET() {
@@ -32,8 +28,8 @@ export async function DELETE(req) {
     mongoose.connect(process.env.MONGO_URL)
     const url = new URL(req.url)
     const _id = url.searchParams.get('_id')
-    if(await isAdmin()) {
-        await MenuItem.deleteOne({_id})
-    }
+  
+    await MenuItem.deleteOne({_id})
+    
     return Response.json(true)
 }
