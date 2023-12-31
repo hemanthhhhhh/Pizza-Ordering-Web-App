3aa194f89f43afc029aded703739604a07e7451e
hemanthhhhhh <krishnahemant2005@gmail.com>
2023-12-31 16:23:04 +0530

    nextauthcommit

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -5,35 +5,9 @@ import {User} from '../../../../models/User'
 import { UserInfo } from "@/models/UserInfo";
 import bcrypt from "bcrypt";
 import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import {authOptions} from '../../../../../utils/authOptions'
 
-export const authOptions = {
-  secret: process.env.SECRET,
-  adapter: MongoDBAdapter(clientPromise),
-  providers: [
-      CredentialsProvider({
-          name: 'Credentials',
-          id: 'credentials',
-          credentials: {
-            username: { label: "Email", type: "email", placeholder: "test@example.com" },
-            password: { label: "Password", type: "password" }
-          },
-          async authorize(credentials, req) {
-            const email = credentials?.email
-            const password = credentials?.password
-            
-            mongoose.connect(process.env.MONGO_URL)
-            const user = await User.findOne({email})
-            const passwordOk = user && bcrypt.compareSync(password, user.password)
-            
-            
-            if(passwordOk) {
-              return user
-            }
-            return null
-          }
-        })
-      ],
-    }
+
 const handler = NextAuth(authOptions)
     
 export { handler as GET, handler as POST}
---
+++ utils/authOptions.js
@@ -0,0 +1,35 @@
+import * as mongoose from "mongoose";
+import CredentialsProvider from "next-auth/providers/credentials"
+import {User} from '../../../../models/User'
+import bcrypt from "bcrypt";
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
+
+
+export const authOptions = {
+    secret: process.env.SECRET,
+    adapter: MongoDBAdapter(clientPromise),
+    providers: [
+        CredentialsProvider({
+            name: 'Credentials',
+            id: 'credentials',
+            credentials: {
+              username: { label: "Email", type: "email", placeholder: "test@example.com" },
+              password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+              const email = credentials?.email
+              const password = credentials?.password
+              
+              mongoose.connect(process.env.MONGO_URL)
+              const user = await User.findOne({email})
+              const passwordOk = user && bcrypt.compareSync(password, user.password)
+              
+              
+              if(passwordOk) {
+                return user
+              }
+              return null
+            }
+          })
+        ],
+      }
