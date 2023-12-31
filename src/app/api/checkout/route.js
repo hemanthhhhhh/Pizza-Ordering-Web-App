5d660eb9470047d08b6476dc316de7c3b510e050
hemanthhhhhh <98905333+hemanthhhhhh@users.noreply.github.com>
2023-12-31 15:41:01 +0530

    Update route.js

--- src/app/api/checkout/route.js
+++ src/app/api/checkout/route.js
@@ -20,54 +20,60 @@ export async function POST(req) {
         cartProducts,
         paid: false,
     })
-    const stripeLineItems = []
-    for(const cartProduct of cartProducts) {
-        const productName = cartProduct.name
-        const productInfo = await MenuItem.findById(cartProduct._id)
-        let productPrice = productInfo.basePrice
-        if(productInfo.size) {
-            const size = productInfo.sizes.find(size => size._id.toString() === cartProduct.size._id.toString())
-            productPrice += size.price
-        }
-        if(cartProduct.extras?.length > 0) {
-            for(const cartProductExtraThing of cartProduct.extras) {
-                const productExtras =  productInfo.extraIngredientPrices
-                const extraThingInfo = productExtras.find(extra => extra._id.toString() === cartProductExtraThing._id.toString())
-                productPrice += extraThingInfo.price
-            }
-        }
+     const stripeLineItems = [];
+  for (const cartProduct of cartProducts) {
 
-        stripeLineItems.push({
-            quantity: 1,
-            price_data: {
-                currency: 'USD',
-                product_data: {
-                    name: productName,
-                },
-                unit_amount: productPrice * 100,
-            },
-        })
+    const productInfo = await MenuItem.findById(cartProduct._id);
+
+    let productPrice = productInfo.basePrice;
+    if (cartProduct.size) {
+      const size = productInfo.sizes
+        .find(size => size._id.toString() === cartProduct.size._id.toString());
+      productPrice += size.price;
+    }
+    if (cartProduct.extras?.length > 0) {
+      for (const cartProductExtraThing of cartProduct.extras) {
+        const productExtras = productInfo.extraIngredientPrices;
+        const extraThingInfo = productExtras
+          .find(extra => extra._id.toString() === cartProductExtraThing._id.toString());
+        productPrice += extraThingInfo.price;
+      }
     }
 
-    const stripeSession = await stripe.checkout.sessions.create({
-        line_items: stripeLineItems,
-        mode: 'payment',
-        customer_email: userEmail,
-        success_url: process.env.NEXTAUTH_URL + 'orders/'+OrderDoc._id.toString() + '?clear-cart=1',
-        cancel_url: process.env.NEXTAUTH_URL + 'cart?canceled=1',
-        metadata: {orderId: OrderDoc._id.toString()},
-        payment_intent_data: {
-            metadata: {orderId: OrderDoc._id.toString()},
+    const productName = cartProduct.name;
+
+    stripeLineItems.push({
+      quantity: 1,
+      price_data: {
+        currency: 'USD',
+        product_data: {
+          name: productName,
         },
-        shipping_options: [
-            {
-                shipping_rate_data: {
-                    display_name: 'Delivery_fee',
-                    type: 'fixed_amount',
-                    fixed_amount: {amount: 500, currency: 'USD'},
-                }
-            }
-        ]
-    })
-    return Response.json(stripeSession.url)
+        unit_amount: productPrice * 100,
+      },
+    });
+  }
+
+  const stripeSession = await stripe.checkout.sessions.create({
+    line_items: stripeLineItems,
+    mode: 'payment',
+    customer_email: userEmail,
+    success_url: process.env.NEXTAUTH_URL + 'orders/' + orderDoc._id.toString() + '?clear-cart=1',
+    cancel_url: process.env.NEXTAUTH_URL + 'cart?canceled=1',
+    metadata: {orderId:orderDoc._id.toString()},
+    payment_intent_data: {
+      metadata:{orderId:orderDoc._id.toString()},
+    },
+    shipping_options: [
+      {
+        shipping_rate_data: {
+          display_name: 'Delivery fee',
+          type: 'fixed_amount',
+          fixed_amount: {amount: 500, currency: 'USD'},
+        },
+      }
+    ],
+  });
+
+  return Response.json(stripeSession.url);
 }
