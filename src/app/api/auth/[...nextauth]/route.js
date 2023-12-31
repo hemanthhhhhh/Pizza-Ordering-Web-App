589bb066fb3d385b16a7733ace465a96c24cdc3a
hemanthhhhhh <98905333+hemanthhhhhh@users.noreply.github.com>
2023-12-31 12:18:49 +0530

    Update route.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -5,7 +5,7 @@ import {User} from '../../../../models/User'
 import { UserInfo } from "@/models/UserInfo";
 import bcrypt from "bcrypt";
 
-const authOptions = {
+export const authOptions = {
   secret: process.env.SECRET,
   providers: [
       CredentialsProvider({
@@ -34,4 +34,4 @@ const authOptions = {
     }
 const handler = NextAuth(authOptions)
     
-export { handler as GET, handler as POST, authOptions}
+export { handler as GET, handler as POST}
