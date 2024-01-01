658a6c7d01a567e1073be445e6a221bd8a361b5e
hemanthhhhhh <98905333+hemanthhhhhh@users.noreply.github.com>
2024-01-01 12:19:27 +0530

    Update route.js

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import { getServerSession } from "next-auth";
-import {authOptions} from '../auth/[...nextauth]/route'
+import {authOptions} from '../../../../utils/authOptions'
 import { User } from "@/models/User";
 import {UserInfo} from "@/models/UserInfo"
 
