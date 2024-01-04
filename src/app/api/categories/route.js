408e5ff933874a96cbb9f03634905eb0129ffcee
hemanthhhhhh <98905333+hemanthhhhhh@users.noreply.github.com>
2024-01-04 23:02:15 +0530

    Update route.js

--- src/app/api/categories/route.js
+++ src/app/api/categories/route.js
@@ -1,16 +1,15 @@
 import mongoose from "mongoose"
 import { Category } from "../../../models/Category"
-import {isAdmin} from "../../../../utils/authOptions";
+
 
 export async function POST(req) {
     mongoose.connect(process.env.MONGO_URL)
     const {name} = await req.json()
 
-    if (await isAdmin()) {
+  
         const categoryDoc = await Category.create({name})
         return Response.json(categoryDoc)
-    } else {
-        return Response.json({});
+   
   }
 }
 
@@ -18,9 +17,9 @@ export async function PUT(req) {
     mongoose.connect(process.env.MONGO_URL)
     const {_id, name} = await req.json()
 
-    if (await isAdmin()) {
+   
         await Category.updateOne({_id}, {name})
-    }
+    
     return Response.json(true)
 }
 
@@ -36,9 +35,9 @@ export async function DELETE(req) {
     const url = new URL(req.url)
     const _id = url.searchParams.get('_id')
 
-     if (await isAdmin()) {
+    
         await Category.deleteOne({_id})
-     }
+     
     return Response.json(true)
 }
 
