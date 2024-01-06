90dac6975c6309958e3bbfb4a04e1417322c9d8a
hemanthhhhhh <98905333+hemanthhhhhh@users.noreply.github.com>
2024-01-06 07:39:49 +0530

    Update route.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -7,7 +7,7 @@ import { UserInfo } from "@/models/UserInfo";
 import bcrypt from "bcrypt";
 import { MongoDBAdapter } from "@auth/mongodb-adapter"
 import {authOptions} from '../../../../../utils/authOptions'
-import {isAdmin} from '../../../../../utils/authOptions'
+
 
 
 const handler = NextAuth(authOptions)
